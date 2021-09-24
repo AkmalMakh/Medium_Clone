06422c237320cb3a2834d7f81a1a9533b5d2fa0a
AkmalMakh <akmal.makhmudov57@gmail.com>
2021-09-24 21:14:02 +0900

    working with forms and setting up static foulder

---
+++ blog/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.db.models import fields
+from django.forms import widgets
+from blog.models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    
+    class Meta():
+        model = Post
+        fields = ('author', 'title', 'text')
+
+        widgets =  {
+ 
+            'title': forms.TextInput(attrs={'class':'textinputclass'}),
+             #  we are gonna use styling libruary so that is why class name: editable medium-editor-textarea
+             # we are gonna creat own class will be: textinputclass, postcontent
+            'text': forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        
+        }
+
+class CommentForm(forms.ModelForm):
+
+
+    class Meta():
+
+        model = Comment
+        fields = ('author', 'text')
+
+        widgets =  {
+ 
+            'author': forms.TextInput(attrs={'class':'textinputclass'}),
+            'text': forms.Textarea(attrs={'class':'editable medium-editor-textarea'}) 
+        }
