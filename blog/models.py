f2b99f61af7ad7daa9110a5f72378d5a45400a84
wjmint <jamie060529@gmail.com>
2021-09-25 14:23:58 +0900

    made model Post and Comment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.CharField(max_length=50)
+    content = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=CASCADE)
+
+    def __str__(self):
+        return self.author
