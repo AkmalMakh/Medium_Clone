5e5f7ed36a2e851961d6e2c714a83867b1feffe8
paintingfox <dragonpdwer@gmail.com>
2021-09-25 14:26:10 +0900

    Made models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class Post(models.Models):
+    title = models.CharField(max_length=180)
+    author = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    nick = models.CharField(max_length=50)
+    context = models.CharField(max_length=1000)
+    post = models.ForeignKey(Post, related_name="Comments", on_delete=CASCADE)
+
+    def __str__(self):
+        return self.nick
