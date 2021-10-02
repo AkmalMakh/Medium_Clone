baa9e3d7f5fc3adb8f94084b2d1e7d78ffa74274
wjmint <jamie060529@gmail.com>
2021-10-02 18:35:45 +0900

    need to make templates

--- blog/forms.py
+++ blog/forms.py
@@ -8,9 +8,17 @@ class postForm(forms.ModelForm):
     class Meta():
         model = Post
         fields = ('author', 'title', 'content')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'content': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'})
+        }
 
 class commentForm(forms.ModelForm):
 
     class Meta():
         model = Comment
         fields = ('author', 'content')
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'content': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'})
+        }
