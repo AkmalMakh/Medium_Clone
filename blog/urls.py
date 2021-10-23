66df42fea0a93f705205abfd25bb613930ac9828
wjmint <jamie060529@gmail.com>
2021-10-23 13:44:13 +0900

    made post list : not sure if the listing works

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import postCreateView, commentCreateView, indexView
+from .views import postCreateView, indexView, postListView
 
 app_name = 'basic_app'
 
 urlpatterns = [
+    path('', postListView.as_view(), name='postList'),
     url(r'^create/$', postCreateView.as_view(), name='postCreate'),
     path('about/', indexView.as_view(), name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import fields
-from django.views.generic import(View, TemplateView, CreateView, DetailView)
+from django.views.generic import(View, TemplateView, CreateView, DetailView, ListView)
 from django.views.generic.detail import DetailView
 from datetime import timezone
 from . import models
@@ -12,19 +12,25 @@ class indexView(TemplateView):
 # needs post list class in here as default home page
 
 class postCreateView(CreateView):
-    template_name = 'postCreate.html'
+    template_name = 'blog_app/postCreate.html'
     model = models.Post
     fields = ('title', 'content', 'author')
 
 class postDetailView(DetailView):
     context_object_name = 'post_details'
     model = models.Post
-    template_name = 'templates/postDetail.html'
+    template_name = 'blog_app/postDetail.html'
 
     def get_queryset(self):
         return models.Post.objects.filter(publishTime__lte=timezone.now().order_by('-publishTime'))
+    
+class postListView(ListView):
+    context_object_name = 'posts'
+    template_name = 'blog_app/postList.html'
+    model = models.Post
 
-class commentCreateView(CreateView):
-    template_name = 'commentCreate.html'
-    model = models.Comment
-    fields = ('content', 'author', 'post')
+
+# class commentCreateView(CreateView):
+#     template_name = 'commentCreate.html'
+#     model = models.Comment
+#     fields = ('content', 'author', 'post')
