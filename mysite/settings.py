3211a1a483eda45c677d70979af449004cb39a5f
AkmalMakh <akmal.makhmudov57@gmail.com>
2022-01-07 21:35:33 +0900

    Allow host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e7%!6=6)+pdax^bl)!%apf1!-bwg)_tvw#lw^n7dm+_iwu53f!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.pythonanywhere.com']
 
 
 # Application definition
